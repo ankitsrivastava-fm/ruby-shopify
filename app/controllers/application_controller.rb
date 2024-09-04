311735b22e7d29e0395a478d1cf327d4b9ed72b4
ankitsrivastava-fm <ankit.srivastava@freshworks.com>
2024-09-04 14:59:19 +0530

    Added logic to create tenant id and associate to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_tenant
+
+  private
+  def set_current_tenant
+    if user_signed_in?
+      @current_tenant = current_user.tenant
+    end
+  end
+
 end
