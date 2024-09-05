449479e9c854ef517fa6ee830b4aa1ccb0f14d06
ankitsrivastava-fm <ankit.srivastava@freshworks.com>
2024-09-05 18:07:08 +0530

    Added Store model and associated with User and Tenant

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   belongs_to :tenant
+  has_one :store, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'stores/new'
+  get 'stores/create'
+  get 'stores/show'
   get 'home/index'
   root 'home#index'
   devise_for :users, controllers: { registrations: 'registrations' }
+  resources :stores, only: [:new, :create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
